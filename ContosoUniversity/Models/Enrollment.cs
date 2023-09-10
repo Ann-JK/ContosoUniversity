ddb8fd928fdd8cc13e48fd122a183d98fce3b7be
Ann-JK <annabelkongas@gmail.com>
2023-09-10 14:06:23 +0300

    Add properties to Enrollment class

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -6,7 +6,12 @@
     }
     public class Enrollment
     {
+        public int EnrollmentID { get; set; }
+        public int CourseID {  get; set; }
+        public int StudentID { get; set; }
+        public Grade? Grade { get; set; }
 
-        public
+        public Course Course { get; set; }
+        public Student Student { get; set; }
     }
 }
