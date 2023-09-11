d1bc9ecca71fa3e27fcc53eadf52f4e3e9737f41
Ann-JK <annabelkongas@gmail.com>
2023-09-11 20:57:59 +0300

    Refactored builder

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -6,21 +6,27 @@ using Microsoft.IdentityModel.Tokens;
 namespace ContosoUniversity
 {
 
+    public class Program {
 
+
+        public static void Main(string[] args) {
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<SchoolContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+            var app = builder.Build();
+
+        }
+    }
 
 }
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<SchoolContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
-var app = builder.Build();
 
 var host = CreateHostBuilder(args).Build();
 
