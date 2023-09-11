ec66b0eec869a1719f3aa06da4047b13c26e5acd
Ann-JK <annabelkongas@gmail.com>
2023-09-11 21:35:54 +0300

    added UseEndPoints

--- ContosoUniversity/Program.cs
+++ ContosoUniversity/Program.cs
@@ -40,10 +40,11 @@ namespace ContosoUniversity
 
             app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            )
             app.Run();
 
         }
