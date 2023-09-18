1552ce221791ee7f9bf26de14c063cfbc4b0ae03
Ann-JK <annabelkongas@gmail.com>
2023-09-18 21:35:00 +0300

    ColumnFirstName migration

---
+++ ContosoUniversity/Migrations/20230918183423_ColumnFirstName.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class ColumnFirstName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Student",
+                newName: "First_Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "First_Name",
+                table: "Student",
+                newName: "FirstName");
+        }
+    }
+}
