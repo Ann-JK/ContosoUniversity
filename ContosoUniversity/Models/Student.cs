579ad4e7c2c2844517369b4a8e68122f17a9b905
Ann-JK <annabelkongas@gmail.com>
2023-09-18 21:29:29 +0300

    name annotation

--- ContosoUniversity/Models/Student.cs
+++ ContosoUniversity/Models/Student.cs
@@ -5,7 +5,11 @@ namespace ContosoUniversity.Models
     public class Student
     {
         public int ID { get; set; }
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50)]
         public string FirstName { get; set; }
 
         [DataType(DataType.Date)]
